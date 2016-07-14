eabad65096109712dda172ddf9b467093823aba4
larryhou.retina.imac <larryhou.retina.imac@foxmail.com>
2016-07-14 15:16:52 +0800

    Update

--- flatten.sh
+++ flatten.sh
@@ -18,21 +18,30 @@ sed -i '' 's/-[[:space:]]*/-/g' ${json}
 
 stats=$(jq -r '.data|to_entries|map(.value.stats|keys|join("\n"))|join("\n")' ${json} | sed '/^$/d' |sort| uniq)
 
-echo "Name,Gold,$(echo ${stats} | sed 's/ /,/g')" > ${file}
-jq -r '.data|to_entries|map(select(.value.stats|length >0)|{name:.value.name,gold:.value.gold.total,stats:.value.stats}|tostring)|join("\n")' ${json}\
+effect=$(jq -r '.data|to_entries|map(.value)|map(select(.effect|length>0)|.effect|to_entries|map(.key)|join("\n"))|join("\n")' ${json} | sort | uniq)
+
+echo "Name,Gold,$(echo -e "${stats}\n${effect}" | xargs | sed 's/ /,/g')" > ${file}
+jq -r '.data|to_entries|map(select(.value.stats|length >0)|{name:.value.name,gold:.value.gold.total,stats:.value.stats,effect:.value.effect}|tostring)|join("\n")' ${json}\
 |while read line
 do
 	while read mod
 	do
 		export ${mod}=0
-	done < <(echo -e "${stats}")
+	done < <(echo -e "${stats}\n${effect}")
+	
 	export $(echo ${line} | jq -r '.stats|to_entries|map([.key,.value|tostring]|join("="))[]')
+	RT_ENV=$(echo ${line} | jq -r '.effect|select(.|length>0)|to_entries|map([.key,.value|tostring]|join("="))[]')
+	if [ ! "${RT_ENV}" = "" ]
+	then
+		export ${RT_ENV}
+	fi
+	
 	item=$(echo ${line} | jq -r '[.name,.gold|tostring]|join(",")')
 	printf "${item},"
 	while read mod
 	do
-		printf $(eval echo \${${mod}})
-		printf ','
-	done < <(echo -e "${stats}")
+		printf -- $(eval echo \${${mod}})
+		printf -- ','
+	done < <(echo -e "${stats}\n${effect}"| sed '/^[[:space:]]*$/d')
 	printf '\n'
 done | sed 's/,$//' >> ${file}
